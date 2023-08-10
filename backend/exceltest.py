a2f38a6b38b6d1aea731d734ff501061490db660
ZoyboyX <sherzoyjan@gmail.com>
2023-08-10 18:23:23 -0400

    Excel Reader

--- backend/exceltest.py
+++ backend/exceltest.py
@@ -1,43 +1,29 @@
 import pandas as pd
+import io
 
-def read_text_to_excel(text, output_file):
-    # Split the text into rows based on newline characters
-    rows = text.strip().split('\n')
+def convert_csv_to_excel(csv_text, excel_file_path):
+    # Read the CSV data using pandas
+    data = pd.read_csv(io.StringIO(csv_text), index_col=0)
+    
+    # Create an Excel writer
+    excel_writer = pd.ExcelWriter(excel_file_path)
+    
+    # Write the data to the Excel file
+    data.to_excel(excel_writer, sheet_name='Sheet1')
+    
+    # Save the Excel file
+    excel_writer.save()
 
-    # Extract the header and data from the rows
-    header = rows[0].split('|')
-    data = [row.split('|') for row in rows[2:]]
-
-    # Clean up the header and data
-    header = [col.strip() for col in header[1:-1]]
-    data = [[col.strip() for col in row[1:-1]] for row in data]
-
-    # Create a DataFrame from the data
-    df = pd.DataFrame(data, columns=header)
-
-    # Convert numeric columns (Age, Salary, Bonus) to integers or floats
-    numeric_columns = ['Age', 'Salary', 'Bonus']
-    df[numeric_columns] = df[numeric_columns].apply(pd.to_numeric)
-
-    # Calculate the 'Total Income' column
-    df['Total Income'] = df['Salary'] + df['Bonus']
-
-    # Save the DataFrame to an Excel file
-    df.to_excel(output_file, index=False)
-
-# Sample text response
-sample_text = """
-| Name       | Age | Occupation    | Salary   | Bonus   | Total Income  |
-|------------|-----|---------------|----------|---------|---------------|
-| John       | 30  | Engineer      | 70000    | 5000    | =C2+D2        |
-| Emily      | 28  | Teacher       | 50000    | 3000    | =C3+D3        |
-| Michael    | 35  | Doctor        | 90000    | 8000    | =C4+D4        |
-| Sarah      | 32  | Lawyer        | 80000    | 6000    | =C5+D5        |
-| Robert     | 29  | Software Eng  | 75000    | 5500    | =C6+D6        |
-"""
+# Sample CSV text
+sample_csv_text = """Name,Age,Occupation,Salary,Bonus,Total Income
+John,30,Engineer,70000,5000,=D2+E2
+Emily,28,Teacher,50000,3000,=D3+E3
+Michael,35,Doctor,90000,8000,=D4+E4
+Sarah,32,Lawyer,80000,6000,=D5+E5
+Robert,29,Software Eng,75000,5500,=D6+E6"""
 
 # Output file name
 output_file_name = 'sample_output.xlsx'
 
-# Call the method to read the text into Excel
-read_text_to_excel(sample_text, output_file_name)
+# Call the method to convert the CSV text to Excel
+convert_csv_to_excel(sample_csv_text, output_file_name)
